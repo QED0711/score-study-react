3aa847d346bcd1ddb9443ecad066d936c6a4ac0e
QED0711 <qdizon@gmail.com>
2020-02-20 05:37:58 -0500

    fixed bug in random score index generator

--- src/components/score/AnswerModal.js
+++ src/components/score/AnswerModal.js
@@ -12,7 +12,7 @@ const AnswerModal = ({ setShowAnswerModal, selectedScore }) => {
             <div className="modal-body answer-modal">
                 <h1>{selectedScore.composer}</h1>
                 <h2>{selectedScore.title}</h2>
-                <h3>Date published*: {selectedScore.date}</h3>
+                <h3>*Date published: {selectedScore.date}</h3>
                 <h4>Tags: {selectedScore.tags.join(", ")}</h4>
 
                 <p><em>* Published dates are retrieved directly from IMSLP.org, and may not be accurate or representative of when the piece was actually written. Make sure to do additional research to confirm these dates.</em></p>
--- src/js/randomScore.js
+++ src/js/randomScore.js
@@ -1,5 +1,13 @@
 const randomScore = (scores, setSelectedScore) => {
-    const randIndex = Math.floor(Math.random() * scores.length - 1);
+
+    // if there is only one score available
+    if(scores.length === 1) return setSelectedScore(scores[0])
+    let randIndex = -1
+    while(randIndex < 0){
+        randIndex = Math.round(Math.random() * scores.length - 1, 0);
+    }
+
+    // console.log({numScores: scores.length, randIndex})
     setSelectedScore(scores[randIndex])
 }
 
