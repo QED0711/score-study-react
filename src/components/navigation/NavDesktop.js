d6f38e9fc9dc50fb13d2dc1302b3189b4ba965e1
QED0711 <qdizon@gmail.com>
2020-02-24 05:52:34 -0500

    reset selected score on page changes

--- src/components/navigation/NavDesktop.js
+++ src/components/navigation/NavDesktop.js
@@ -8,16 +8,22 @@ const NavDesktop = () => {
   const { state: s, stateMethods: sm } = useContext(UserContext);
   const {state: cs, stateMethods: csm} = useContext(ComposerContext);
 
+  // HELPERS
   const resetSelectedComposers = () => {
     csm.setSelectedComposers([])
   }
+
+  const resetSelectedScore = () => {
+    csm.setSelectedScore(null)
+  }
+
   return (
     <nav className="navbar navbar-desktop">
       {!s.user && <Link to="/sign-in">Sign In</Link>}
       <Link to="/">About</Link>
-      <Link to="/app">Application</Link>
+      <Link to="/app" onClick={resetSelectedScore} >Application</Link>
       {/* {!s.user && <Link to="/create-account">Create Account</Link>} */}
-      {s.user && <Link to="/comments">Your Comments</Link>}
+      {s.user && <Link to="/comments" onClick={resetSelectedScore} >Your Comments</Link>}
       {s.user && <Link to="/account-settings">Account Settings</Link>}
       {s.user && <Link to="/logout">Logout</Link>}
       {s.user && s.user.authorization === "admin" && <Link to="/admin">Admin</Link>}
